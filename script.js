26344fa868cb26fc31d8be6573f63e0d97a707fe
Isa-2001 <isabellatrog2001@gmail.com>
2026-02-09 22:37:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
+let db;
+const request = indexedDB.open("appDUA", 1);
+
+request.onupgradeneeded = (event) => {
+  db = event.target.result;
+  if (!db.objectStoreNames.contains("turmas")) {
+    db.createObjectStore("turmas", { keyPath: "id" });
+  }
+};
+
+request.onsuccess = (event) => {
+  db = event.target.result;
+  carregarTurmas();
+};
+
+function salvarTurma(turma) {
+  const tx = db.transaction("turmas", "readwrite");
+  const store = tx.objectStore("turmas");
+  store.add(turma);
+}
+
+function carregarTurmas() {
+  const tx = db.transaction("turmas", "readonly");
+  const store = tx.objectStore("turmas");
+  const request = store.getAll();
+
+  request.onsuccess = () => {
+    renderizarTurmas(request.result);
+  };
+}
+
 const btnNovaTurma = document.getElementById("btnNovaTurma");
 const listaTurmas = document.getElementById("listaTurmas");
-
-let turmas = [];
 
 btnNovaTurma.addEventListener("click", () => {
   const nome = prompt("Nome da turma:");
   if (!nome) return;
 
-  turmas.push({ id: Date.now(), nome });
-  renderizarTurmas();
+  const turma = {
+    id: Date.now(),
+    nome: nome
+  };
+
+  salvarTurma(turma);
+  carregarTurmas();
 });
 
-function renderizarTurmas() {
+function renderizarTurmas(turmas) {
   listaTurmas.innerHTML = "";
   turmas.forEach(turma => {
     const li = document.createElement("li");
